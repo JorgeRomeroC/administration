3acb39ce1753f8c06079d8aacd8c2897f114e2e5
Jorge Romero C <jorgeromeroc@MacBook-Pro-de-Jorge.local>
2024-09-30 00:34:52 -0300

    se incorpora una plantilla para el login del sistema

--- applications/user/forms.py
+++ applications/user/forms.py
@@ -6,6 +6,19 @@ from .models import User
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
+        fields = ['email', 'password']  # Especifica los campos que quieres en el formulario
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserChangeForm, self).__init__(*args, **kwargs)
+        # Añadir clases de Bootstrap a los campos
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',  # Clases de Bootstrap para estilizar
+            'placeholder': 'Email',
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Contraseña',
+        })
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
